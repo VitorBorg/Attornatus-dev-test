226bc4effff3e54814a20fb236cbcd8233dbc04e
Vitor Borges <vitor27.sab@hotmail.com>
2023-01-15 20:42:13 -0300

    String response class created

--- src/main/java/com/vitorBorg/Attornatus/utils/exception/Response/StringResponse.java
+++ src/main/java/com/vitorBorg/Attornatus/utils/exception/Response/StringResponse.java
@@ -1,2 +1,12 @@
-package com.vitorBorg.Attornatus.utils.exception.Response;public class StringResponse {
+package com.vitorBorg.Attornatus.utils.exception.Response;
+
+public class StringResponse {
+    private String response;
+
+    public StringResponse(String response) {
+        this.response = response;
+    }
+    public String getResponse() {
+        return response;
+    }
 }
